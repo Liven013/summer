6c45a2b8c6ab06ac3b4cfa21b712105aa619b327
Liven013 <dania.kwasow@yandex.ru>
2025-01-06 23:14:21 +0300

    create MVC pattern struction

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from database.database import init_db
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    init_db()
+    uvicorn.run("main:app", host="localhost", port = 8080, reload = True)
---
+++ app/models/db/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .camp_shift import CampShift
+from .organization import Organization
